452bd6dba577f834103bdcccbb9bc399499ece9f
june4539 <kjune@bithumblive.com>
2022-11-11 14:41:01 +0900

    feat: init project

---
+++ src/main/java/com/woowahan/thdeng/test/jacoco/JavaFoo.java
@@ -0,0 +1,21 @@
+package com.woowahan.thdeng.test.jacoco;
+
+public class JavaFoo {
+
+    public String hello(String name) {
+//        switch (name) {
+////            case "펭":
+////                return "하";
+////            case "hello":
+////                return "world";
+//            default:
+//                return "no one";
+//        }
+        return "하";
+    }
+
+//    public void callMe() {
+//        System.out.println("Please, call me");
+//    }
+
+}
