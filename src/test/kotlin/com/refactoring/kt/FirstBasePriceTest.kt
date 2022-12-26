4d338f672b71b7ee718fd070863f3755654eb462
june <kimjy4536@hotmail.com>
2022-12-26 16:09:22 +0900

    feat: add test code

--- src/test/kotlin/com/refactoring/kt/FirstBasePriceTest.kt
+++ src/test/kotlin/com/refactoring/kt/FirstBasePriceTest.kt
@@ -6,18 +6,18 @@ import org.junit.jupiter.api.Test
 class FirstBasePriceTest {
 
 
-//    @Test
-//    internal fun `basePrice 1000 이상일때`() {
-//        val firstBasePrice = FirstBasePrice(1000, 2)
-//        var restult = firstBasePrice.getPrice()
-//        Assertions.assertEquals(restult, 1900.0)
-//    }
-//
+   @Test
+   internal fun `basePrice 1000 이상일때`() {
+       val firstBasePrice = FirstBasePrice(1000, 2)
+       var restult = firstBasePrice.getPrice()
+       Assertions.assertEquals(restult, 1900.0)
+   }
 
-//    @Test
-//    internal fun `basePrice 1000 이하일때`() {
-//        val firstBasePrice = FirstBasePrice(10, 2)
-//        var restult = firstBasePrice.getPrice()
-//        Assertions.assertEquals(restult, 19.6)
-//    }
+
+   @Test
+   internal fun `basePrice 1000 이하일때`() {
+       val firstBasePrice = FirstBasePrice(10, 2)
+       var restult = firstBasePrice.getPrice()
+       Assertions.assertEquals(restult, 19.6)
+   }
 }
